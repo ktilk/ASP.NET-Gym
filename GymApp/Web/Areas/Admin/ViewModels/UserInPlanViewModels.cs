b54040c5d13a59217ab7ea42a75a299f276e6836
Kaspar Tilk <kaspartilk2@gmail.com>
2016-05-09 17:18:36 +0300

    puuduolev vaatemudel

---
+++ GymApp/Web/Areas/Admin/ViewModels/UserInPlanViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain;
+
+namespace Web.Areas.Admin.ViewModels
+{
+    public class UserInPlanIndexViewModel
+    {
+        public List<UserInPlan> UserInPlans { get; set; }
+    }
+    public class UserInPlanCreateEditViewModel
+    {
+        public UserInPlan UserInPlan { get; set; }
+        public SelectList UserSelectList { get; set; }
+        public SelectList PlanSelectList { get; set; }
+        public SelectList UserRoleInPlanSelectList { get; set; }
+    }
+}
